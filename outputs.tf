dc6f588fbb6acd35899b144a1cf3f672c71b188c
Nicholas Joanisse <nicholas@rosecape.ca>
2024-10-16 09:24:53 -0400

    reorder: alphabetical order

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,6 @@
-output "principal_public_subnet_id" {
-  description = "The ID of the principal public subnet."
-  value       = azurerm_subnet.principal_public.id
-}
-
-output "principal_public_subnet_address_prefixes" {
-  description = "The address prefixes of the principal public subnet."
-  value       = azurerm_subnet.principal_public.address_prefixes
-}
-
-output "principal_private_subnet_id" {
-  description = "The ID of the principal private subnet."
-  value       = azurerm_subnet.principal_private.id
-}
-
-output "principal_private_subnet_address_prefixes" {
-  description = "The address prefixes of the principal private subnet."
-  value       = azurerm_subnet.principal_private.address_prefixes
+output "principal_database_subnet_address_prefixes" {
+  description = "The address prefixes of the principal database subnet."
+  value       = azurerm_subnet.principal_database.address_prefixes
 }
 
 output "principal_database_subnet_id" {
@@ -23,9 +8,24 @@ output "principal_database_subnet_id" {
   value       = azurerm_subnet.principal_database.id
 }
 
-output "principal_database_subnet_address_prefixes" {
-  description = "The address prefixes of the principal database subnet."
-  value       = azurerm_subnet.principal_database.address_prefixes
+output "principal_private_subnet_address_prefixes" {
+  description = "The address prefixes of the principal private subnet."
+  value       = azurerm_subnet.principal_private.address_prefixes
+}
+
+output "principal_private_subnet_id" {
+  description = "The ID of the principal private subnet."
+  value       = azurerm_subnet.principal_private.id
+}
+
+output "principal_public_subnet_address_prefixes" {
+  description = "The address prefixes of the principal public subnet."
+  value       = azurerm_subnet.principal_public.address_prefixes
+}
+
+output "principal_public_subnet_id" {
+  description = "The ID of the principal public subnet."
+  value       = azurerm_subnet.principal_public.id
 }
 
 output "virtual_network_address_space" {
--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
-variable "environment" {
-  description = "The environment that the resources are created in"
-  type        = string
-}
-
 variable "create_virtual_network" {
   description = "A flag to create a virtual network"
   type        = bool
 }
 
-variable "virtual_network_name" {
-  description = "The name of the virtual network"
+variable "environment" {
+  description = "The environment that the resources are created in"
   type        = string
 }
 
-variable "virtual_network_location" {
-  description = "The location/region where the virtual network is created"
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "subnet_name_prefix" {
+  description = "The prefix that is used for the subnet names"
   type        = string
 }
 
@@ -24,12 +24,12 @@ variable "virtual_network_address_space" {
   default     = null
 }
 
-variable "subnet_name_prefix" {
-  description = "The prefix that is used for the subnet names"
+variable "virtual_network_location" {
+  description = "The location/region where the virtual network is created"
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group"
+variable "virtual_network_name" {
+  description = "The name of the virtual network"
   type        = string
 }
